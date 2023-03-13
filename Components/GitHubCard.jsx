554b4084726cb3fb13daaf1eb02e57cdcac2d32f
Laila Rimando <lrimando7@gmail.com>
2023-03-12 20:45:32 -0700

    Snapshot Test

---
+++ Components/GitHubCard.jsx
@@ -0,0 +1,19 @@
+import Card from 'react-bootstrap/Card'
+import React from 'react'
+import CuteDog from './CuteDog.png'
+
+const GitHubCard = () => {
+    <div>
+        <Card>
+            <Card.Img src = {cute dog} alt="cute dog" />
+            <Card.Body>
+                <Card.Title>Hello</Card.Title>
+                <Card.Text>
+                    Hello World!
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </div>
+}
+
+export default GitHubCard; 
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GitHubCard from './Github/GitHubCard'
+
+function App() {
+  return (
+    <div className="App">
+      <h2> SnapShot</h2>
+    </div>
+  );
+}
+
+export default App;
